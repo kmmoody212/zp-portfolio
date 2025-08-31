ac3fd3eade9bbd2344ae2d64b397c17e9c9f2055
Katrina Moody <kmfoster212@gmail.com>
2025-08-31 19:17:36 -0400

    Installed TagCloud library to use for skills section and made sure no issues rendering. Need to style.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import "./style.css";
 import {loadPortfolio} from "./pages/portfolio";
-import { loadSkills } from "./pages/skills";
+// import { loadSkills } from "./pages/skills";
+import TagCloud from "TagCloud";
+
+const container = '.tagcloud';
+
+const texts = [
+    'PR', 'example1', 'example2', 'example3',
+]
+
+const options = {
+    radius: 140,
+    maxSpeed: 'fast',
+    initSpeed: 'normal',
+    direction: 135,
+    keep: true,
+}
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   // You can pass a selector string or the actual element
+//   const instance = TagCloud('#sphere', texts, options);
+// });
+
+  // example: click handler via event delegation
+  document.querySelector('#sphere').addEventListener('click', (e) => {
+    if (e.target?.classList.contains('tagcloud--item')) {
+      console.log('clicked:', e.target.textContent);
+    }
+  });
 
 loadPortfolio();
-loadSkills();
+// loadSkills();
+
+TagCloud(container, texts, options);
