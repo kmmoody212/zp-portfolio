b6069402f55096bf0d57f4a2dbd88f18da6fb820
Katrina Moody <kmfoster212@gmail.com>
2025-09-15 19:33:06 -0400

    Added font awesome icons for mobile menu. Created logic for mobile menu dropdown. Edited CSS for difference vw's.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import "./style.css";
 import {loadPortfolio} from "./pages/portfolio";
 import { renderTagcloud } from "./pages/skills";
 import { loadSkills } from "./pages/skills";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 
 
 
 loadPortfolio();
 loadSkills();
 renderTagcloud();
+
+
+   
